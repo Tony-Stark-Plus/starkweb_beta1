622a46e852d52450d2f25c40fb09d41b55404847
xieshengyu <xieshengyu@xieshengyudeMacBook-Pro.local>
2018-08-31 11:20:54 +0800

    fix ico miss bug

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="starkweb icon" href="/img/title_img3.ico"/>
     <title>Starkweb</title>
     @yield("css")
-    <link rel="starkweb icon" href="http://ozz0bm6k3.bkt.clouddn.com/Starkweb/img/title_img3.ico"/>
     <link href="https://cdn.bootcss.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="/css/app.css">
     <link href="https://cdn.bootcss.com/devicons/1.8.0/css/devicons.min.css" rel="stylesheet">
