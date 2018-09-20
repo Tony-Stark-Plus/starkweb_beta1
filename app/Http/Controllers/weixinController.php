78637069b75ef5d62db7e178831541c68ebf5fec
xieshengyu <xieshengyu@xieshengyudeMacBook-Pro.local>
2018-09-20 22:48:07 +0800

    weixin api3

--- app/Http/Controllers/weixinController.php
+++ app/Http/Controllers/weixinController.php
@@ -9,7 +9,7 @@ class weixinController extends Controller
 {
     //
     public function index(){
-        $articleList = articleList::latest()->get(['id', 'aname','kind_class','created_at'])->take(15);
+        $articleList = articleList::latest()->get(['id', 'aname','kind_class','created_at','kind'])->take(15);
         $this->date_to_en($articleList);
         return $articleList;
     }
