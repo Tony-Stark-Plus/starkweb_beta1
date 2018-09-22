203302a9cf89583578feb65e654b1d3ffd224713
xieshengyu <xieshengyu@xieshengyudeMacBook-Pro.local>
2018-09-22 21:02:26 +0800

    weixin api4

--- app/Http/Controllers/weixinController.php
+++ app/Http/Controllers/weixinController.php
@@ -8,9 +8,17 @@ use App\articleList;
 class weixinController extends Controller
 {
     //
-    public function index(){
-        $articleList = articleList::latest()->get(['id', 'aname','kind_class','created_at','kind'])->take(15);
+    public function index(Request $request)
+    {
+        //文章总数
+        $article_num = articleList::count();
+        //分页需要的页数
+        $page_num =ceil($article_num/10);
+        $articleList = articleList::latest()->skip(($request['page']-1)*10)->take(10)->get(['id', 'aname', 'kind_class', 'created_at', 'kind']);
         $this->date_to_en($articleList);
-        return $articleList;
+        //Api重构
+        $data['page_num']=$page_num;
+        $data['articleList']=$articleList;
+        return $data;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -53,4 +53,4 @@ Route::get('/404',function(){
 Route::get('/hljs',function(){
     return view('title');
 });
-Route::get('/weixin','weixinController@index');
+Route::get('/weixin/articleList/{page}','weixinController@index');
